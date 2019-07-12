5e49151aa9b8b81ef908d63d71332b13d711d151
vdarapan <vdarapan@kent.edu>
2019-07-12 01:29:06 -0400

    Changes

--- static/p5_translate_example.js
+++ static/p5_translate_example.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function setup() {
     createCanvas(400,400);
 }
@@ -8,15 +7,5 @@ function draw() {
     fill(255);
     translate(50, 50);
     rect(0,0,100,50);
-=======
-function setup() {
-    createCanvas(400,400);
-}
 
-function draw() {
-    background(0);
-    fill(255);
-    translate(50, 50);
-    rect(0,0,100,50);
->>>>>>> ac4f01e39558da94ac7a93e86b64e6ed60c97f53
 }
--- static/p5js_1a.js
+++ static/p5js_1a.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let angle = 0;
 
 function setup() {
@@ -27,34 +27,4 @@ function draw() {
 
     angle = angle + 5;
    // rect(50, 50, 100, 50);
-=======
-let angle = 0;
-
-function setup() {
-    createCanvas(400,400);
-    angleMode(DEGREES);
-    rectMode(CENTER);
-}
-
-function draw() {
-    background(0);
-    
-    push();
-    translate(50, 50);
-    rotate(angle);
-    fill(255, 100, 50);
-    rect(0, 0, 50, 50);
-    pop();
-
-    translate(300, 300);
-    rotate(-angle * 3);
-    fill(50, 100, 255);
-    rect(0,0,100,50);
-    
-   // translate(mouseX, mouseY);
-    //rect(0,0,100,50);
-
-    angle = angle + 5;
-   // rect(50, 50, 100, 50);
->>>>>>> ac4f01e39558da94ac7a93e86b64e6ed60c97f53
 }
